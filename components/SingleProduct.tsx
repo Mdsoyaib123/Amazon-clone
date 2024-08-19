1f627ed19a26e22799248bb67ed5435b0da6a758
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2024-08-19 12:06:52 +0600

    added file

--- components/SingleProduct.tsx
+++ components/SingleProduct.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Rating from "./shered/Rating";
 
 const SingleProduct = ({ singleProduct }: { singleProduct: any }) => {
   return (
@@ -7,15 +8,22 @@ const SingleProduct = ({ singleProduct }: { singleProduct: any }) => {
       <div>
         {singleProduct.map((product: any) => {
           return (
-            <div key={product.id}>
-              <Image
-                
+            <div key={product.id} className="flex ">
+              <div className="bg-gray-100"><Image
+                className='mix-blend-multiply p-4'
                 src={product.image}
-                height={400}
-                width={250}
+                height={500}
+                width={400}
                 alt={product.title}
-              />
-              <h1>{product.title}</h1>
+              /></div>
+              
+              <div>
+              <h1 className="font-bold text-lg">{product.title}</h1>
+              <p>{product.description}</p>
+              <Rating ratings={product.rating}></Rating>
+              <p className='font-bold text-2xl'>{`$${product.price}`}</p>
+              </div>
+             
             </div>
           );
         })}
