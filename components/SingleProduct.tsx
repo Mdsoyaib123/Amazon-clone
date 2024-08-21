11885e2e6b454815b2e4b8d80166c09440449007
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2024-08-21 11:44:25 +0600

    added file

--- components/SingleProduct.tsx
+++ components/SingleProduct.tsx
@@ -8,7 +8,7 @@ const SingleProduct = ({ singleProduct }: { singleProduct: any }) => {
       <div>
         {singleProduct.map((product: any) => {
           return (
-            <div key={product.id} className="flex ">
+            <div key={product.id} className="flex gap-4 ">
               <div className="bg-gray-200"><Image
                 className='mix-blend-multiply p-4'
                 src={product.image}
@@ -22,6 +22,11 @@ const SingleProduct = ({ singleProduct }: { singleProduct: any }) => {
               <p>{product.description}</p>
               <Rating ratings={product.rating}></Rating>
               <p className='font-bold text-2xl'>{`$${product.price}`}</p>
+              <div>
+                <h1 className="font-bold text-sm">About this item </h1>
+                <ul className='list-disc ml-5'><li>Cell Service Booster: This phone booster antenna ensures fewer dropped calls, better voice quality, faster mobile internet, and improved streaming for a single mobile device in a cradle mount</li>
+                <li>Portable Cell Signal Booster: This mobile cell phone booster supports the latest in 5G technology and has tool-free installation, guided by our app</li></ul>
+              </div>
               </div>
              
             </div>
