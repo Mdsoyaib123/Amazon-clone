1aefa55f8be3538448c01edcfc08f3548c5865a8
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2024-08-22 20:37:15 +0600

    update redux

--- redux/cartSlice.ts
+++ redux/cartSlice.ts
@@ -11,6 +11,10 @@ const cartSlice =createSlice({
     initialState,
     reducers:{
         // multiple action 
-        
+        addToCart:(state,action)=>{
+            state.cart.push(action.payload)
+        }
     }
 })
+export const {addToCart} = cartSlice.actions;
+export default cartSlice.reducer;
