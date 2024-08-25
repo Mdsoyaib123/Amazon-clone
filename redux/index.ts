78a4994f1df4144eeaa91fe399d25654f169c0a8
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2024-08-25 14:40:05 +0600

    install redux persist

--- redux/index.ts
+++ redux/index.ts
@@ -1,6 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit'
-import cartSlice from './cartSlice'
 
+import cartSlice from './cartSlice'
+import { configureStore } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 // ...
 
 export const store = configureStore({
