6bb7113cc64208eef9541f20ee316ceafc2e7f6a
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2024-08-26 12:45:50 +0600

    added file

---
+++ components/ProccedToBuy.tsx
@@ -0,0 +1,10 @@
+
+const ProccedToBuy = () => {
+  return (
+    <div>
+      <p>Your order is eligible for FREE Delivery .Choose FREE Delivery option at checkout</p> 
+    </div>
+  )
+}
+
+export default ProccedToBuy
--- components/cart.tsx
+++ components/cart.tsx
@@ -1,13 +1,14 @@
+import ProccedToBuy from "./ProccedToBuy"
 import ShoppingCart from "./ShoppingCart"
 
 const Cart = () => {
   return (
     <div className='flex gap-6 pt-10 mx-8'>
-      <div className="w-[80%]  ">
+      <div className="w-[75%]  ">
       <ShoppingCart></ShoppingCart>
     </div>
-    <div className='w-[20%] bg-white rounded-md text-center p-4 h-fit'>
-      <h1>Payment section</h1>
+    <div className='w-[25%] bg-white rounded-md text-center p-4 h-fit'>
+     <ProccedToBuy></ProccedToBuy>
     </div>
     </div>
     
