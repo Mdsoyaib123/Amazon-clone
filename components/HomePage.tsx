6c6bfad77193a6dc4d55f559cfe3dd409e053e5c
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2024-08-27 13:32:59 +0600

    creat home page

---
+++ components/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image';
+
+const HomePage = () => {
+  return (
+    <div>
+      {/* <Image src={'https://m.media-amazon.com/images/I/71ve0pYz7uL._SX3000_.jpg'} width={100} height={100 } alt={'abc'}/> */}
+      <h1>Home Page </h1>
+    </div>
+  )
+}
+export default HomePage;
