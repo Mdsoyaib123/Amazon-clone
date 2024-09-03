53b3a28f89efff0f99fb70a1d8bde83c4de39a9f
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2024-09-04 01:13:38 +0600

    working on home page

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -19,18 +19,18 @@ const HomePage = () => {
     getJewelryProduct();
     getElectronicsProduct()
   }, []);
-
+  console.log()
   
   return (
-    <div>
+    <div className='bg-gray-100'>
       <Image
         src={"https://i.postimg.cc/wBFqvrjh/71ve0p-Yz7u-L-SX3000.jpg"}
         width={1500}
-        height={1000}
+        height={2000}
         quality={90}
         alt={"abc"}
       />
-      <div className="w-[80%] mx-auto grid grid-cols-4 gap-4">
+      {/* <div className="w-[80%] mx-auto grid grid-cols-4 gap-4"> */}
         {/* {mensProduct.map((product: any) => {
           return (
             <Link href={`./product/${product.id}`} key={product.id}>
@@ -39,10 +39,21 @@ const HomePage = () => {
             </Link>
           );
         })} */}
-        <div>
-
+        <div className="  grid grid-cols-4 justify-center items-center gap-3 px-4  relative -top-72">
+          <div className='flex justify-center w-[300px] h-[400px] mx-auto border  px-20 py-24 bg-white'>
+            <Image src={mensProduct[3]?.image} width={200 } height={200} quality={80}alt={'abc'}/>
+          </div>
+          <div className='flex justify-center  w-[300px] h-[400px] mx-auto border px-20 py-24 bg-white'>
+          <Image src={womansProduct[4]?.image} width={200 } height={100} quality={80} alt={'abc'}/>
+          </div>
+          <div className='flex justify-center  w-[300px] h-[400px] mx-auto border px-20 py-24 bg-white'>
+          <Image src={jewelry[1]?.image} width={200 }  height={200} quality={80} alt={'abc'}/>
+          </div>
+          <div className='flex justify-center w-[300px] h-[400px] mx-auto border px-20 py-24 bg-white'>
+          <Image src={electronics[4]?.image} width={200 } height={200} quality={80} alt={'abc'}/>
+          </div>
         </div>
-      </div>
+      {/* </div> */}
     </div>
   );
 };
